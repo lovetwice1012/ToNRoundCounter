8f177aa085368139f608f58d9bf6265e58854ec7
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2025-09-11 10:43:35 +0900

    Implement IDisposable for HttpClientWrapper

--- Infrastructure/HttpClientWrapper.cs
+++ Infrastructure/HttpClientWrapper.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,11 +9,13 @@ namespace ToNRoundCounter.Infrastructure
     /// <summary>
     /// Default implementation of <see cref="IHttpClient"/> using <see cref="HttpClient"/>.
     /// </summary>
-    public class HttpClientWrapper : IHttpClient
+    public class HttpClientWrapper : IHttpClient, IDisposable
     {
         private readonly HttpClient _client = new HttpClient();
 
         public Task<HttpResponseMessage> PostAsync(string url, HttpContent content, CancellationToken cancellationToken)
             => _client.PostAsync(url, content, cancellationToken);
+
+        public void Dispose() => _client.Dispose();
     }
 }
